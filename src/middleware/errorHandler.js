82bcb1739f6a384d616ed7f3c7e514f4b7e07ec6
patrickglobalai <patrickdahdal8008@gmail.com>
2024-11-29 15:42:28 +0000

    Create errorHandler.js

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+export const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err);
+
+  res.status(500).json({
+    error: 'Internal Server Error',
+    message: err.message
+  });
+};
